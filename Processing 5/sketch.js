12d7fa1ed6e1339d72a5d4132918ca084a9dcb42
Sadip Giri <sadipgiri@bennington.edu>
2018-09-14 15:14:33 -0400

    some changes

--- Processing 5/sketch.js
+++ Processing 5/sketch.js
@@ -8,7 +8,8 @@ var canvasWidth = 640;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  img = createImg('https://avatars3.githubusercontent.com/u/14111239?s=400&u=450f6389a09cb00e31f51947f2aeb983db278a24&v=4');
+  // img = createImg('https://avatars3.githubusercontent.com/u/14111239?s=400&u=450f6389a09cb00e31f51947f2aeb983db278a24&v=4');
+  img = createImg('https://hacked.com/wp-content/uploads/2018/09/fake-news.jpg');
   img.hide();
 }
 
@@ -16,6 +17,7 @@ function draw() {
 	width = 250;
 	height = 250;
   image(img, canvasWidth/3, canvasHeight/3, width, height); 
-	textSize(32);
-	text('Sadip Giri', 250, 100);
+	textSize(27);
+  // text('Sadip Giri', 250, 100);
+  text('Credibility & Fact Checking?', 200, 100);
 }
